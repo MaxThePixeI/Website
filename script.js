9040137b940a25ea93f0d66543f4c39ecfba1673
MaxThePixeI <mailhelp08@gmail.com>
2024-08-17 22:47:39 +0200

    Simple website Ui with sidebar
    All free

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the entire page to load
+window.addEventListener('load', function() {
+    // Add a class to body when the page is fully loaded
+    document.body.classList.add('loaded');
+});
